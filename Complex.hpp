a8015a01bd924c4b434e133f56e19b0c5ca751c5
Kavern Siegl Sinaga <111416701+vernthedragon@users.noreply.github.com>
2026-06-18 09:53:59 +0700

    Refactor Complex class for inline functions and error handling

--- Complex.hpp
+++ Complex.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include <cmath>
 
+#if defined(_MSC_VER)
+#define __COMPLEXFORCEINLINE __forceinline
+#else
+#define __COMPLEXFORCEINLINE [[gnu::always_inline]]
+#endif
+
 template <typename Var>
 struct Complex {
     Var re;
@@ -19,6 +25,8 @@ struct Complex {
         return Complex(this->re * scalar, this->im * scalar);
     }
     inline Complex operator/(const Var& scalar) const {
+        if (scalar == 0)
+            return *this;
         return Complex(this->re / scalar, this->im / scalar);
     }
     inline Complex operator+(const Var& scalar) const {
@@ -28,27 +36,29 @@ struct Complex {
         return Complex(this->re - scalar, this->im);
     }
     Complex operator/(const Complex& other) const {
-#ifndef MATHUSEINVERSEAPPROX
+
+        if (other.re == 0 && other.im == 0)  // ##! error handling for zero division in complex
+            return *this;
+
         return Complex( 
             ((this->re * other.re) + (this->im * other.im)) / ((other.re * other.re) + (other.im * other.im)), 
             ((this->im * other.re) - (this->re * other.im)) / ((other.re * other.re) + (other.im * other.im))
         );
-#endif
     }
-    __forceinline bool operator==(const Complex& other) const {
+    __COMPLEXFORCEINLINE bool operator==(const Complex& other) const {
         return this->re == other.re && this->im == other.im;
     }
-    __forceinline bool operator!=(const Complex& other) const {
+    __COMPLEXFORCEINLINE bool operator!=(const Complex& other) const {
         return !(this->operator==(other));
     }
     void operator=(const Complex& other) {
         this->re = other.re;
         this->im = other.im;
     }
-    __forceinline Var tan() const { return static_cast<Var>(this->im / this->re); }
-    __forceinline Var ang() const { return static_cast<Var>(atan2(this->im, this->re)); } //angle ##! improve performance with faster atan2
-    __forceinline Var abs() const { return static_cast<Var>(sqrt(this->re * this->re + this->im * this->im)); }
-    __forceinline Var abssqr() const { return (this->re * this->re + this->im * this->im); }
+    __COMPLEXFORCEINLINE Var slope() const { return static_cast<Var>(this->im / this->re); }
+    __COMPLEXFORCEINLINE Var ang() const { return static_cast<Var>(atan2(this->im, this->re)); } //angle ##! improve performance with faster atan2
+    __COMPLEXFORCEINLINE Var abs() const { return static_cast<Var>(sqrt(this->re * this->re + this->im * this->im)); }
+    __COMPLEXFORCEINLINE Var abssqr() const { return (this->re * this->re + this->im * this->im); }
 };
 
 
